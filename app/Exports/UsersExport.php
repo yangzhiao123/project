90b49dcb8c19edc68b2a6c4c306133818a866800
yangzhiao123 <zhiao.yang@bioon.com>
2018-09-06 17:44:36 +0800

    up

--- app/Exports/UsersExport.php
+++ app/Exports/UsersExport.php
@@ -4,11 +4,22 @@ namespace App\Exports;
 
 use App\Model\UserModel;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;    //设置标题
 
-class UsersExport implements FromCollection
+
+class UsersExport implements FromCollection, WithHeadings
 {
     public function collection()
     {
-        return UserModel::where('id','1');
+        return UserModel::all();
+    }
+
+    public function headings():array
+    {
+        return [
+            '姓名',
+            '邮箱',
+            '密码',
+        ];
     }
 }
--- app/Http/Controllers/ExcelController.php
+++ app/Http/Controllers/ExcelController.php
@@ -8,6 +8,12 @@ use App\Http\Controllers\Controller;
 use App\Model\UserModel;
 use App\Exports\UsersExport;
 use Maatwebsite\Excel\Excel;
+use PhpParser\ErrorHandler\Collecting;
+use Maatwebsite\Excel\Concerns\WithHeadings;    //设置标题
+use Maatwebsite\Excel\Concerns\WithStrictNullComparison;   //为空时零填充
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;      //自动单元格尺寸
+use PhpOffice\PhpSpreadsheet\Style\NumberFormat;      //设置单元格数据格式
+use Maatwebsite\Excel\Concerns\WithColumnFormatting;       //设置列格式
 
 class ExcelController extends Controller
 {
